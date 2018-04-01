2c56bdd0a9dd66c42c08208a830f3e3c1ada6bd6
ashikap89@gmail.com <ashikap89@gmail.com>
2018-04-01 16:32:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ var takeANumber = function(katzDeli, newCustomer) {
   return "Currently serving " + first + "."
 }
 
+function currentLine(line) {
+  var lineNum = [];
+   if (line.length == 0) {
+     return "The line is currently empty.";
+   }
+   for (var i = 0; i < line.length; i++) {
+     lineNum.push(` ${i+1}. ${line[i]}`);
+ 	}
+   return "The line is currently:" + lineNum;
+
+ }
