1533b5cc743f36e78ce4375eb7e45f6811033228
ashikap89@gmail.com <ashikap89@gmail.com>
2018-04-01 16:30:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ var takeANumber = function(katzDeli, newCustomer) {
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
   
-  
+  function nowServing(lineArr){
+  if (lineArr.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  var first = lineArr[0]
+  lineArr.shift()
+  return "Currently serving " + first + "."
+}
