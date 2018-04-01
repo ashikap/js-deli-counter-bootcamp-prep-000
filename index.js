fab02209940f93c7e51b1d4038634ea560687df6
ashikap89@gmail.com <ashikap89@gmail.com>
2018-04-01 16:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var takeANumber = function(katzDeli, newCustomer) {
      katzDeli.push(newCustomer);
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
+  
+  
